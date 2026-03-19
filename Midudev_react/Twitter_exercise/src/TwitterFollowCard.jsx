277095a2cad82eb7deb1eadc8b0f5419099ef814
RobNR1458 <roberto-ne-ro@hotmail.com>
2026-03-18 21:05:51 -0600

    Se añadió animación de 'dejar de seguir' al momento de hacer hover sobre el botón

--- Midudev_react/Twitter_exercise/src/TwitterFollowCard.jsx
+++ Midudev_react/Twitter_exercise/src/TwitterFollowCard.jsx
@@ -26,7 +26,8 @@ export function TwitterFollowCard({ userName, name }) {
       </header>
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
         </button>
       </aside>
     </article>
