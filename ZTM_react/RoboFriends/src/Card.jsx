0f2fcc9d51a35765d8e2658f2e59c1b1a2d2b024
RobNR1458 <roberto-ne-ro@hotmail.com>
2026-03-26 16:02:36 -0600

    Reestructuración del working tree

---
+++ ZTM_react/RoboFriends/src/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return (
+    <div>
+      <img alt="Avatar" src="" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+}
