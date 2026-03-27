f06cf25cbf292c1f76e9f57315e0b4d1b0d2cab0
RobNR1458 <roberto-ne-ro@hotmail.com>
2026-03-26 22:12:25 -0600

    Componente de cartas funcional añadido

--- ZTM_react/RoboFriends/src/Card.jsx
+++ ZTM_react/RoboFriends/src/Card.jsx
@@ -1,10 +1,10 @@
-export default function Card() {
+export default function Card({ name, username, email }) {
   return (
-    <div>
-      <img alt="Avatar" src="" />
+    <div className="bg-light-green dib br5 pa3 ma2 grow bw2 shadow-5">
+      <img alt="Avatar" src={`https://robohash.org/${username}?200*200`} />
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
--- ZTM_react/RoboFriends/src/main.jsx
+++ ZTM_react/RoboFriends/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Card from "./Card.jsx";
+import App from "./App.jsx";
+import "tachyons";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Card />
+    <>
+      <App />
+    </>
   </StrictMode>,
 );
