388f65cc182694b9e1465191b7660a4025b8e0eb
RobNR1458 <roberto-ne-ro@hotmail.com>
2026-03-30 14:45:35 -0600

    Refactoricé el código para introducir un api call para obtener la lista de robots (Anteriormente la lista de robots era hardcodeada.

--- ZTM_react/RoboFriends/src/App.jsx
+++ ZTM_react/RoboFriends/src/App.jsx
@@ -1,37 +1,29 @@
-import { robots } from "./robots.js";
 import CardList from "./CardList.jsx";
 import SearchBox from "./SearchBox.jsx";
 import { useState } from "react";
 import "./App.css";
 
-const state = {
-  robo: robots,
-  searchfield: "",
-};
-
 export default function App() {
-  const [field, setField] = useState(state);
+  const [searchField, setSearchField] = useState("");
+  const [robots, setRobots] = useState([]); //Ahora robots no estará hardcodeado, se hará una api call
 
   //Esta función se encarga de manejar el cambio de estado cada que el usuario teclea algo nuevo en el input box
-  const handleChange = (foo) => {
-    //Primero formatearemos la cadena de texto recibida para que este en minúsculas...
-    //... de esta forma normalizamos la búsqueda independientemente si se teclea en mayúsculas, además el método 'includes' es case sensitive
-    const fooLower = foo.toLowerCase();
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(fooLower);
-    });
-    const newState = { robo: filteredRobots, searchfield: foo };
-    setField(newState);
+  const handleInputChange = (currentInputText) => {
+    setSearchField(currentInputText);
   };
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
 
   return (
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
       <SearchBox
-        inputText={field.searchfield}
-        OnInputTextChange={handleChange}
+        inputText={searchField}
+        OnInputTextChange={handleInputChange}
       />
-      <CardList robots={field.robo} />
+      <CardList robots={filteredRobots} />
     </div>
   );
 }
